e95aca7169ea8fa6c702289c5f0478201f2c7382
Abilov Jeyhun <abilovv599@gmail.com>
2023-02-16 21:53:15 +0400

    slice added to the store

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -2,8 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import userAuthSlice from "../slice/userAuthSlice";
 import getCardData from "../slice/getCardDataSlice";
+import getCardDataById from "../slice/getCardDataById";
 
 export const store = configureStore({
-  reducer: { userAuthSlice: userAuthSlice, getCardData: getCardData },
+  reducer: {
+    userAuthSlice: userAuthSlice,
+    getCardData: getCardData,
+    getCardDataById: getCardDataById,
+  },
   middleware: [thunk],
 });
