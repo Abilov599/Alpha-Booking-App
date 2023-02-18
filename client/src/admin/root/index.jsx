f2062eb668a69c287a8de68d4f1a9a7a2384f96f
Abilov Jeyhun <abilovv599@gmail.com>
2023-02-18 16:31:42 +0400

    admin access

--- client/src/admin/root/index.jsx
+++ client/src/admin/root/index.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserAuth } from "../../redux/slice/userAuthSlice";
 
 const AdminRoot = () => {
-  return <Outlet />;
+  const { loading, data, error } = useSelector((state) => state.userAuthSlice);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUserAuth());
+  }, [dispatch]);
+
+  return data?.isAdmin ? <Outlet /> : <h1>Access Denied</h1>;
 };
 
 export default AdminRoot;
