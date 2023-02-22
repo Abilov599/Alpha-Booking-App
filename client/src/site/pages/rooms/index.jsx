3d13eef7c8f4ae91ed80730affba8512640e6ec2
Abilov Jeyhun <abilovv599@gmail.com>
2023-02-22 19:33:51 +0400

    only logged user can access rooms!

--- client/src/site/pages/rooms/index.jsx
+++ client/src/site/pages/rooms/index.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./index.scss";
 import RoomCard from "./../../components/room-card/index";
 import { Helmet } from "react-helmet";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserAuth } from "../../../redux/slice/userAuthSlice";
 
 const Rooms = () => {
-  return (
+  const { loading, data, error } = useSelector((state) => state.userAuthSlice);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUserAuth());
+  }, [dispatch]);
+
+  const useAuth = () => {
+    return data && data;
+  };
+
+  const isAuth = useAuth();
+  return isAuth ? (
     <main id="rooms">
       <Helmet>
         <meta charSet="utf-8" />
@@ -30,6 +44,8 @@ const Rooms = () => {
         </div>
       </section>
     </main>
+  ) : (
+    <Link to="/sign-in">Log in First</Link>
   );
 };
 
