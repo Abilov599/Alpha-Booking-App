7f623ea73204862f09249768698d1c24d651a686
Abilov Jeyhun <abilovv599@gmail.com>
2023-02-24 17:02:06 +0400

    Booking schema created!

---
+++ server/models/Booking.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const bookingSchema = new Schema(
+  {
+    room: { type: String, required: true },
+    roomId: { type: String, required: true },
+    userId: { type: String, required: true },
+    transactionId: { type: String, required: true },
+    checkInDate: { type: String, required: true },
+    checkOutDate: { type: String, required: true },
+    totalDays: { type: Number, required: true },
+    totalAmount: { type: Number, required: true },
+    childCount: { type: Number, required: true },
+    adultCount: { type: Number, required: true },
+    status: { type: String, required: true, default: "booked" },
+  },
+  { timestamps: true }
+);
+
+export const Booking = mongoose.model("booking", bookingSchema);
