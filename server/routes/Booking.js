e24eaf62a55203e88503acd144f8e05a34683e2f
Abilov Jeyhun <abilovv599@gmail.com>
2023-02-24 17:05:49 +0400

    Booking routes created!

---
+++ server/routes/Booking.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/bookings", );
+router.get("/bookings/:id", );
+router.post("/bookings", );
+router.delete("/bookings/:id", );
+
+export { router as BookingRoute };
