e693cfcf6917f3ae8ddb560b6470df7295001f76
Abilov Jeyhun <abilovv599@gmail.com>
2023-03-01 07:31:46 +0400

    server js configured for base64

--- server/server.js
+++ server/server.js
@@ -6,12 +6,12 @@ import mongoose from "mongoose";
 import { RoomRoute } from "./routes/Room.js";
 import { UserRoute } from "./routes/User.js";
 import { BookingsRoute } from "./routes/Booking.js";
-// import { multer } from "multer";
 
 //App config
 const app = express();
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 dotenv.config();
 mongoose.set("strictQuery", false);
